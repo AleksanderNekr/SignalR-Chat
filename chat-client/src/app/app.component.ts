59f0fc4a5a8e664c93f41f1c32d9a970a09388ef
aleks <aleks@email.noreply.com>
2023-12-30 16:50:54 +0500

    [Feature] Add chat component.

--- chat-client/src/app/app.component.ts
+++ chat-client/src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { SignalrService } from "./services/signalr.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'chat-client';
+export class AppComponent implements OnInit {
+  title = 'chat-client'
+
+  messages: Message[] = []
+  user = ''
+
+  inputNameForm = new FormGroup({
+    'user': new FormControl('', Validators.required)
+  })
+  sendMessageForm = new FormGroup({
+    'text': new FormControl('', Validators.required)
+  })
+
+  constructor(private readonly chatService: SignalrService,
+              private readonly changeDetector: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.chatService.startConnection()
+    this.chatService.addChatMsgListener((user, text) => {
+      this.messages.push({ user, text })
+      this.changeDetector.markForCheck()
+    })
+  }
+
+  public sendMessage() {
+    let text = this.sendMessageForm.get('text')?.value ?? '<empty>'
+    this.chatService.sendMessage(this.user, text)
+    this.sendMessageForm.reset()
+  }
+
+  saveUser() {
+    this.user = this.inputNameForm.get('user')?.value ?? '<empty>'
+    this.changeDetector.markForCheck()
+  }
+}
+
+interface Message {
+  user: string
+  text: string
 }
