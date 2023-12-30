e6e311cb521d7027eab1c7b685dac3c0fef5b8b2
aleks <aleks@email.noreply.com>
2023-12-30 15:36:57 +0500

    [Refactor] Reinit client app.

--- chat-client/src/app/app.component.spec.ts
+++ chat-client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'chat-app' title`, () => {
+  it(`should have the 'chat-client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chat-app');
+    expect(app.title).toEqual('chat-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chat-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chat-client');
   });
 });
